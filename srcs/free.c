cbb46c97852fe21765ad63c11b06b742a83a2da6
seprovv <gregorymtl@outlook.com>
2024-04-09 00:48:54 +0200

    add

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: grmortel <grmortel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 05:15:41 by grmortel          #+#    #+#             */
-/*   Updated: 2024/04/01 05:38:29 by grmortel         ###   ########.fr       */
+/*   Updated: 2024/04/09 00:44:06 by grmortel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	free_stack(t_stack *stack)
 	t_node *head_stack;
 	t_node	*next;
 
-	t_node head_stack = stack->head;
-	while (current)
+	t_node head_stack = stack->top;
+	while (head_stack)
 	{
 		next = head_stack->next;
 		free(head_stack);
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,15 +6,37 @@
 /*   By: grmortel <grmortel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 01:38:16 by grmortel          #+#    #+#             */
-/*   Updated: 2024/04/08 13:46:40 by seprovv          ###   ########.fr       */
+/*   Updated: 2024/04/09 00:47:45 by grmortel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pushswap"
 
-int	stack_exist(t_stack *stack_a)
+int	check_stack(t_stack *stack_a, int i)
 {
 	t_node *curent;
 
-	current =  
+	current = stack_a->head;
+	while(current)
+	{
+		if (current->value == i)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
+
+void	stack_is_valid(t_stack *stack_a)
+{
+	t_node *current;
+	t_node *next;
+
+	current = stack_a->head;
+	while (current->next)
+	{
+		next = current->next;
+		if (current->value == next->value)
+			write(2, "Error\n", 6);
+		current = current->next;
+	}
 }
